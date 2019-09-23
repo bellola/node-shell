71e69f703cde80ab1478b0a72c94c3ee91013d03
Luis Bello <luisbg9@gmail.com>
2019-09-23 13:07:58 -0400

    added ls file

--- bash.js
+++ bash.js
@@ -1,10 +1,11 @@
 const pwd = require('./pwd');
-
+const ls = require('./ls')
 process.stdout.write("prompt > ");
 
 process.stdin.on('data', (data) =>{
     const cmd = data.toString().trim();
     pwd(cmd);
+    ls(cmd);
     // if (cmd === 'pwd') {
     //   process.stdout.write(`${process.cwd()}`);
     //   process.stdout.write('\nprompt > ')
